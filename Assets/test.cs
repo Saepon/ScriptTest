d9bfea47b269d0f0f79fde252cd9a17ae02c8af7
Saepon <s.haisa.0426@gmail.com>
2020-06-18 19:36:27 +0900

    ScriptTest
    宜しくお願い致します

---
+++ Assets/test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = new int[5]; //変数arrayの宣言＆arrayという名の5要素配列を定義
+        array[0] = 64; // 5要素それぞれ定義
+        array[1] = 38;
+        array[2] = 65;
+        array[3] = 75;
+        array[4] = 87;
+
+
+        for (int x = 0; x <= 4; x++)
+        {
+            Debug.Log (array[x]);
+        }
+
+        for (int x = 4; x >= 0; x--)
+        {
+            Debug.Log(array[x]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
